5f33cf28acc0afafaefbe8ff4412f09d98e0e5a7
AaronW <aaronwhildkhild@yahoo.com>
2021-11-17 11:56:32 -0600

    Add project files.

---
+++ Greeter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Greeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
